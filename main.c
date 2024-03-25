7e60c82dccbb36038f005fc27d61c1095d293fe7
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-03-25 18:30:53 +0000

    resolutions

--- main.c
+++ main.c
@@ -12,37 +12,81 @@
 
 #include "init.h"
 
-// int ext_checker(char *arg)
-// {
-//
-// }
-
-void parse()
+void	map_checker(int ac, char *map)
 {
-	
-}
-
-int main(int ac, char **av)
-{
-	int fd;
-	
 	//check the number of args
 	if (ac != 2)
 	{
 		printf("more/less arguments");
-		return 0;
+		exit(1);
 	}
 	//args validation
-	if (ft_strlen(av[1]) <= 4 || !ft_strnstr(av[1], ".fdf", ft_strlen(av[1])))	
+	if (ft_strlen(map) <= 4 || !ft_strnstr(map, ".fdf", ft_strlen(map)))
 	{
 		printf("invalid file");
-		return 0;
+		exit(1);
 	}
-	fd = open(av[1], O_RDONLY);
+}
+
+
+int rows_count(char *line)
+{
+	int rows;
+	
+	rows = 0;
+	while (*line)
+	{
+		while (*line && *line == ' ')
+			line++;
+		if (*line)
+			rows++;
+		while (*line && *line != ' ')
+			line++;
+	}
+	return rows;
+}
+
+void cols_rows_count(char *map, int *x, int *y)
+{
+	int fd;
 	char *str;
+
+
+
+	str = NULL;
+	fd = open(map, O_RDONLY);
+	if (fd == -1)
+		exit(1);
 	while ((str = get_next_line(fd)))
 	{
-		printf("%s\n", str);
+		*x = rows_count(str); 
+		(*y)++;
 	}
-	
+	printf("rows %d", *x);
 }
+
+int main(int ac, char **av)
+{
+	t_pcord **cords;
+	int fd;
+	char *str;
+
+	int i = 0;
+	int x = 0;
+	int y = 0;
+
+	// validate the map
+	map_checker(ac, av[1]);
+
+	// width and height of the map
+	cols_rows_count(av[1], &x, &y);
+}
+
+
+// int main(int ac, char **av)
+// {
+// 	int fd = open(av[1], O_RDONLY);
+// 	char *str;
+// 	while ((str = get_next_line(fd)))
+// 		printf("%s", str);
+// }
